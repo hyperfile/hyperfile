85bf5be49aacf5f2d8de059c6680f1005330a729
daiyy <daiyiyang@hotmail.com>
2025-03-07 00:32:32 +0000

    add examples

---
+++ examples/hyper.rs
@@ -0,0 +1,121 @@
+use std::io::{Result, ErrorKind};
+use std::ops::Range;
+use rand::Rng;
+use aws_sdk_s3::Client;
+use hyperfile::file::hyper::Hyper;
+use hyperfile::file::flags::FileFlags;
+
+const NUM_ITER: usize = 100;
+const DEFAULT_BLOCK_SIZE: usize = 4096;
+const DEFAULT_MAX_FILE_SIZE: usize = 50 * 1024 * 1024; // 50MiB
+const DEFAULT_RANDOM_WRITE_OFFSET: Range<usize> = 0..DEFAULT_MAX_FILE_SIZE;
+const DEFAULT_RANDOM_WRITE_BYTES: Range<usize> = 1..DEFAULT_BLOCK_SIZE*1024; // 1Byte to 4MiB
+
+async fn random_write(client: &Client, uri: &str, data: &mut Vec<u8>) -> Result<()> {
+    let rand_data_offset = rand::random_range(DEFAULT_RANDOM_WRITE_OFFSET);
+    let mut rand_data_bytes = rand::random_range(DEFAULT_RANDOM_WRITE_BYTES);
+    if rand_data_offset + rand_data_bytes >= DEFAULT_MAX_FILE_SIZE {
+        rand_data_bytes = DEFAULT_MAX_FILE_SIZE - rand_data_offset;
+    }
+    let total_len = rand_data_offset + rand_data_bytes;
+    // only resize buffer for data extend case
+    if total_len > data.len() {
+        data.resize(total_len, 0);
+    }
+    print!("random write offset {} bytes {} ..", rand_data_offset, rand_data_bytes);
+
+    let flags = FileFlags::rdwr();
+    let mut hyper = Hyper::fs_open_or_create(client, uri, flags).await?;
+    // write content
+    let filled_buf = &mut data[rand_data_offset..total_len];
+    let write_bytes = hyper.fs_write(rand_data_offset, filled_buf).await?;
+    assert!(write_bytes == rand_data_bytes);
+    let last_cno = hyper.fs_release().await?;
+    println!(". Done with last cno {}", last_cno);
+    Ok(())
+}
+
+async fn random_truncate(client: &Client, uri: &str, data: &mut Vec<u8>) -> Result<()> {
+    let curr_len = data.len();
+    // ratio to truncate only one byte
+    let new_file_len = if rand::rng().random_ratio(2, 10) {
+        curr_len - 1
+    } else if rand::rng().random_ratio(2, 10) {
+        curr_len + 1
+    } else {
+        rand::random_range(0..DEFAULT_MAX_FILE_SIZE)
+    };
+    print!("random truncate file to new size {} ..", new_file_len);
+    // open file for read
+    let flags = FileFlags::rdwr();
+    let mut hyper = Hyper::fs_open_or_create(&client, &uri, flags).await?;
+    // get stat
+    let stat = hyper.fs_getattr()?;
+    assert!(stat.st_size == data.len() as i64);
+    if new_file_len > stat.st_size as usize {
+        print!(".[EXPEND]..");
+    } else if new_file_len < stat.st_size as usize {
+        print!(".[SHRINK]..");
+    } else {
+        print!(".[NO CHANGE]..");
+    }
+
+    // extend file by truncate
+    hyper.fs_truncate(new_file_len).await?;
+    let last_cno = hyper.fs_release().await?;
+    data.resize(new_file_len, 0);
+    println!(". Done with last cno {}", last_cno);
+    Ok(())
+}
+
+async fn read_check(client: &Client, uri: &str, data: &mut Vec<u8>) -> Result<()> {
+    let total_bytes = data.len();
+    // open file for read
+    let flags = FileFlags::rdonly();
+    let mut hyper = Hyper::fs_open_or_create(client, uri, flags).await?;
+    // get stat
+    let stat = hyper.fs_getattr()?;
+    assert!(stat.st_size == total_bytes as i64);
+
+    let mut buf = Vec::new();
+    buf.resize(total_bytes, 0);
+    let read_bytes = hyper.fs_read(0, &mut buf).await?;
+    println!("real data in memory md5: {:?} - read data from file md5: {:?}", md5::compute(&data), md5::compute(&buf));
+    assert!(read_bytes == total_bytes);
+    let _last_cno = hyper.fs_release().await?;
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    env_logger::init();
+	let uri = std::env::var("HYPERFILE_STAGING_ROOT_URI").unwrap();
+
+    // build s3 client
+	let config = aws_config::load_from_env().await;
+    let client = aws_sdk_s3::Client::new(&config);
+
+    // clear any existing on staging
+    let _ = Hyper::fs_unlink(&client, &uri).await
+                        .or_else(|e| {
+                            if e.kind() == ErrorKind::NotFound {
+                                return Ok(());
+                            }
+                            Err(e)
+                        })?;
+
+    let mut data = Vec::new();
+
+    for _ in 0..NUM_ITER {
+        if rand::rng().random_ratio(7, 10) {
+            random_write(&client, &uri, &mut data).await?;
+        } else {
+            random_truncate(&client, &uri, &mut data).await?;
+        }
+    }
+
+    println!("Final read check...");
+    read_check(&client, &uri, &mut data).await?;
+
+    Ok(())
+}
