3b9e9892c50b5cb9f2ec3bdf0690933d0b1fb81b
daiyy <daiyiyang@hotmail.com>
2025-08-31 01:11:17 +0000

    redefine wal trait

--- src/wal/mod.rs
+++ src/wal/mod.rs
@@ -1,17 +1,26 @@
 use std::pin::Pin;
 use std::io::Result;
+use std::collections::BTreeMap;
 use crate::SegmentId;
-use crate::buffer::BatchDataBlockWrapper;
 
 pub mod config;
 pub(crate) mod s3;
 
-pub(crate) trait WalReadWrite {
+// ondisk chunk desc
+#[derive(Debug)]
+pub struct WalChunkDesc {
+    pub seq: usize,
+    pub key: String,
+    pub offset: usize,
+    pub len: usize,
+    pub is_zero: bool,
+}
+
+pub trait WalReadWrite {
     // write
     fn write(&mut self, segid: SegmentId, offset: usize, buf: &[u8]) -> Pin<Box<dyn Future<Output = Result<()>> + Send + 'static>>;
     fn write_zero(&mut self, segid: SegmentId, offset: usize, len: usize) -> Pin<Box<dyn Future<Output = Result<()>> + Send + 'static>>;
     // reader
-    fn collect_segments(&self) -> Pin<Box<dyn Future<Output = Result<Vec<SegmentId>>> + Send + '_>>;
-    #[allow(dead_code)]
-    fn collect(&self, segid: SegmentId) -> Pin<Box<dyn Future<Output = Result<Vec<BatchDataBlockWrapper>>> + Send + '_>>;
+    fn list_segments(&self) -> Pin<Box<dyn Future<Output = Result<Vec<SegmentId>>> + Send + '_>>;
+    fn list_chunks(&self, segid: SegmentId) -> Pin<Box<dyn Future<Output = Result<BTreeMap<usize, WalChunkDesc>>> + Send + '_>>;
 }
