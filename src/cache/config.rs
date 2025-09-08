584856e6f6a838d0c628bbaac9a1bd66e25f66f1
daiyy <daiyiyang@hotmail.com>
2025-09-08 02:26:28 +0000

    add missing file for cache config

---
+++ src/cache/config.rs
@@ -0,0 +1,61 @@
+use std::io::{Error, ErrorKind, Result};
+use std::path::Path;
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, Default, PartialEq, Deserialize, Serialize)]
+pub struct MemCacheConfig {}
+
+#[derive(Clone, Debug, Default, PartialEq, Deserialize, Serialize)]
+pub struct LocalDiskCacheConfig {
+    pub cache_dir: Option<String>,
+    pub cache_file_path: Option<String>,
+}
+
+const DEFAULT_CACHE_DIR: &str = "/tmp";
+
+impl LocalDiskCacheConfig {
+    pub fn full_file_path(&self) -> Result<String> {
+        let cache_dir_str = self.cache_dir.clone().unwrap_or(DEFAULT_CACHE_DIR.to_string());
+        let cache_dir = Path::new(&cache_dir_str);
+        if !cache_dir.try_exists()? {
+            return Err(Error::new(ErrorKind::NotFound, "cache dir {cache_dir} for LocalDiskCache not found"));
+        }
+
+        let cache_file_path = self.cache_file_path.clone().unwrap_or(ulid::Ulid::new().to_string());
+        if let Some(s) = cache_dir.join(cache_file_path).as_path().to_str() {
+            return Ok(s.to_string());
+        }
+        return Err(Error::new(ErrorKind::InvalidInput, "unable to get cache file"));
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Deserialize, Serialize)]
+pub enum HyperFileCacheConfig {
+    Memory(MemCacheConfig),
+    LocalDisk(LocalDiskCacheConfig),
+}
+
+impl Default for HyperFileCacheConfig {
+    fn default() -> Self {
+        Self::Memory(MemCacheConfig::default())
+    }
+}
+
+impl HyperFileCacheConfig {
+    pub fn new_mem() -> HyperFileCacheConfig {
+        Self::Memory(MemCacheConfig::default())
+    }
+
+    pub fn new_local_disk(cache_dir: Option<&str>, cache_file_path: Option<&str>) -> HyperFileCacheConfig {
+        Self::LocalDisk(
+            LocalDiskCacheConfig {
+                cache_dir: cache_dir.and_then(|s| Some(s.to_string())),
+                cache_file_path: cache_file_path.and_then(|s| Some(s.to_string())),
+            }
+        )
+    }
+
+    pub fn new_local_disk_default() -> HyperFileCacheConfig {
+        Self::new_local_disk(None, None)
+    }
+}
